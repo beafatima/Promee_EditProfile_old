e56fadd9c8149bd3d99d523d4abc5dd4f1904669
dings <dings@LAPTOP-5L4EH1QH>
2023-10-22 12:41:24 +0800

    Add project files.

---
+++ Promee_EditProfile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Promee_EditProfile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Promee_EditProfile/MainPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿namespace Promee_EditProfile
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void OnImage1Tapped(object sender, TappedEventArgs e)
+        {
+
+            var next = new NewPage();
+            await Navigation.PushAsync(next);
+        }
+        private async void OnButtonClicked(object sender, EventArgs args)
+        {
+            var next1 = new NewPage();
+            await Navigation.PushAsync(next1);
+        }
+    }
+}
